fe4bc83604b66df4285cdb6e8a26a605005c8d47
GSU <gsu1234@mail.ru>
2018-11-16 14:03:18 +0900

    php unit

--- tests/Protocol/Transfer/TransferTest.php
+++ tests/Protocol/Transfer/TransferTest.php
@@ -34,685 +34,161 @@ class TransferTest extends TestCase
         $this->assertInstanceOf(FileTransfer\Protocol\Transfer\SSH2Transfer\SSH2SFTP::class, $connSSH);
     }
 
-    public function testSetFolderCurrentSize()
-    {
-        $factory = new FT\Factory();
-
-    }
-
-    public function testSetFolderCurrentCountFiles()
-    {
-
-    }
-
-
-    public function testSetFolderDefaultCurrentSize()
-    {
-
-    }
-
-    public function testSetFolderDefaultCurrentCountFiles()
-    {
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    public function testAddToCache($data, $arTags)
-    {
-
-    }
-
-    public function testCheckCache($arTags)
-    {
-
-    }
-
-    public function testClearAllCache()
-    {
-    }
-
-    public function testClearCacheByTag($arTags)
-    {
-
-    }
-
-    public function testGetCacheTags($arAdditionalsTags)
-    {
-
-    }
-
-
-    ///////////////////////////////////////////////////////////////////
-    public function testPrepareArguments()
-    {
-
-    }
-
-    public function testCheckIsTransferFunction()
-    {
-    }
-
-
-    public function testCheckIsHightLevelFunction()
-    {
-    }
-
-    ///////////////////////////////////////////////////////////////////
-
-    public function testSetDefaultParameters()
-    {
-
-    }
-
-    ///////////////////////////////////////////////////////////////////
-
     public function testSetConnectionParameters()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
+        $connFTP->setConnectionParameters(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3]
+        );
+
+        $this->assertEquals($connFTP->getConnectionType(), $this->arFTPConnectionSettings[0]);
+        $this->assertEquals($connFTP->getConnectionUser(), $this->arFTPConnectionSettings[1]);
+        $this->assertEquals($connFTP->getConnectionPass(), $this->arFTPConnectionSettings[2]);
+        $this->assertEquals($connFTP->getConnectionHostname(), $this->arFTPConnectionSettings[3]);
 
     }
 
-    public function testGetConnectionType()
-    {
-    }
-
-    public function testGetConnectionUser()
-    {
-    }
-
-    public function testGetConnectionPass()
-    {
-
-    }
-
-    public function testGetConnectionHostname()
-    {
-    }
     ///////////////////////////////////////////////////////////////////
 
     public function testEnablePassiveConnectionMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
+
+        $this->assertTrue($connFTP->enablePassiveConnectionMode());
+        $this->assertTrue($connFTP->isPassiveMode());
     }
 
     public function testDisablePassiveConnectionMode()
     {
-    }
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
 
-    public function testIsPassiveMode()
-    {
+        $this->assertFalse($connFTP->disablePassiveConnectionMode());
+        $this->assertFalse($connFTP->isPassiveMode());
     }
 
     public function testSetDefaultConnectionMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
+
+        $connFTP->setDefaultConnectionMode();
+        $this->assertTrue($connFTP->isPassiveMode());
     }
 
     ///////////////////////////////////////////////////////////////////
 
     public function testSetBinaryTransferMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
+
+        $this->assertEquals($connFTP->setBinaryTransferMode(), \FTP_BINARY);
+        $this->assertEquals($connFTP->getTransferMode(), \FTP_BINARY);
     }
 
     public function testSetTextualTransferMode()
     {
-    }
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
 
-    public function testGetTransferMode()
-    {
+        $this->assertEquals($connFTP->setTextualTransferMode(), \FTP_ASCII);
+        $this->assertEquals($connFTP->getTransferMode(), \FTP_ASCII);
     }
 
     public function testSetDefaultTransferMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
+
+        $connFTP->setDefaultTransferMode();
+        $this->assertEquals($connFTP->getTransferMode(), \FTP_BINARY);
     }
+
 
     ///////////////////////////////////////////////////////////////////
 
-    public function testSetSwapMode($strSwapMode)
+    public function testSetSwapMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
 
+        $connFTP->setSwapMode("copy");
 
-    }
+        $this->assertEquals($connFTP->getSwapMode(), "copy");
 
-    public function testGetSwapMode()
-    {
     }
 
     public function testSetDefaultSwapMode()
     {
+        $factory = new FT\Factory();
+        $connFTP = $factory->getConnection(
+            $this->arFTPConnectionSettings[0],
+            $this->arFTPConnectionSettings[1],
+            $this->arFTPConnectionSettings[2],
+            $this->arFTPConnectionSettings[3],
+            $this->arFTPConnectionSettings[4]
+        );
 
+        $connFTP->setDefaultSwapMode();
+        $this->assertEquals($connFTP->getSwapMode(), "copy");
     }
 
     ///////////////////////////////////////////////////////////////////
 
-    public function testEnableShowProgress()
-    {
-    }
-
-    public function testDisableShowProgress()
-    {
-    }
-
-    public function testGetShowProgress()
-    {
-    }
-
-    public function testSetDefaultShowProgress()
-    {
-
-    }
-
-    public function testGetCurrentShowProgressPrefix()
-    {
-
-    }
-
-
-    ///////////////////////////////////////////////////////////////////
-
-    public function testSearch(
-    ) {
-
-
-    }
-
-    ///////////////////////////////////
-    private function testActionListPreparingBeforeSync()
-    {
-
-    }
-
-    private function testActionListPreparingAfterSync()
-    {
-
-    }
-
-    private function testActionListSync($strFolder)
-    {
-
-    }
-
-    private function testActionTransferPreparingBeforeSync()
-    {
-
-    }
-
-    private function testActionTransferPreparingAfterSync()
-    {
-
-    }
-
-    private function testActionTransferSync()
-    {
-
-    }
-
-
-    ///////////////////////////////////
-
-    private function testSearchingPreparingBefore()
-    {
-
-    }
-
-    private function testSearchingPreparingAfter()
-    {
-
-    }
-
-    ///////////////////////////////////
-    public function testDownloadFolder($strRemoteFolder, $strLocalFolder = "")
-    {
-
-    }
-
-    public function testUploadFolder($strLocalFolder, $strRemoteFolder = "")
-    {
-
-    }
-
-    ///////////////////////////////////
-    public function testPreparingTransferFolder($strFolder)
-    {
-
-    }
-
-    public function testPreparingTransferFolderNB($strFolder)
-    {
-
-    }
-
-    ///////////////////////////////////
-    public function testSetLocalFolderSize($strLocalFolder)
-    {
-
-
-    }
-
-    public function testSetRemoteFolderSize($strRemoteFolder)
-    {
-
-    }
-
-    public function testGetFolderSize()
-    {
-    }
-
-    ///////////////////////////////////
-    public function testSetLocalFolderCountFiles($strLocalFolder)
-    {
-
-
-    }
-
-    public function testSetRemoteFolderCountFiles($strRemoteFolder)
-    {
-
-
-    }
-
-    public function testGetFolderCountFiles()
-    {
-    }
-
-    ///////////////////////////////////
-    public function testSync($strOriginFolder, $strReceiverFolder = "")
-    {
-
-    }
-
-    /////////////////////////////////////////////
-    public function testClearFullNamesFilesList()
-    {
-    }
-
-    public function testGetFullNamesFilesList()
-    {
-    }
-
-    public function testPrintFullNamesFilesList()
-    {
-    }
-
-    public function testPutToFullNamesFilesList($strFileFullName)
-    {
-    }
-
-    public function testClearCurrentPathOrigin()
-    {
-    }
-
-    public function testGetCurrentPathOrigin()
-    {
-    }
-
-    public function testAddToCurrentPathOrigin($strFolderOrFile)
-    {
-    }
-
-    public function testRemoveLastElementFormCurrentPathOrigin()
-    {
-    }
-
-
-
-    public function testClearCurrentPathReceiver()
-    {
-    }
-
-    public function testGetCurrentPathReceiver()
-    {
-    }
-
-    public function testAddToCurrentPathReceiver($strFolderOrFile)
-    {
-
-    }
-
-    public function testRemoveLastElementFormCurrentPathReceiver()
-    {
-
-    }
-
-
-    public function testDisableMkDirLogMode()
-    {
-    }
-
-    public function testEnableMkDirLogMode()
-    {
-    }
-
-    public function testIsMkDirLogMode()
-    {
-    }
-
-
-    ///////////////////////////////////
-    public function testSyncChdirOrigin($strFolder)
-    {
-
-    }
-
-    public function testSyncChdirReceiver($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirReceiver($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirOrigin($strFolder)
-    {
-
-    }
-
-    public function testSyncMkDir($strFolder)
-    {
-
-    }
-
-    public function testSyncNlist($strFolder)
-    {
-
-    }
-
-    public function testSyncAction($file, $strActionMethodName)
-    {
-
-    }
-
-    public function testSyncTransfer($file)
-    {
-
-    }
-
-    public function testSyncFolderTransferNB($file)
-    {
-
-    }
-
-    public function testSyncFolderTransfer()
-    {
-
-    }
-
-    public function testSyncList($file)
-    {
-
-    }
-
-    ///////////////////////////////////
-    public function testSyncSwapFileCopy($file)
-    {
-    }
-
-    public function testSyncSwapFileMove($file)
-    {
-
-    }
-
-    public function testSyncSwapFolderCopy($folder)
-    {
-
-    }
-
-    public function testSyncSwapFolderMove($folder)
-    {
-
-    }
-    ///////////////////////////////////
-    public function testSyncChdirOriginDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncChdirOriginUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncChdirReceiverDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncChdirReceiverUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirReceiverDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirReceiverUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirOriginDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncIsDirOriginUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncMkDirDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncMkDirUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncNlistDownload($strFolder)
-    {
-
-    }
-
-    public function testSyncNlistUpload($strFolder)
-    {
-
-    }
-
-    public function testSyncTransferDownload($file)
-    {
-    }
-
-    public function testSyncTransferUpload($file)
-    {
-    }
-
-    public function testSyncFileTransferNB($file, $strDirection)
-    {
-    }
-
-    public function testSyncFileTransfer($file, $strDirection)
-    {
-    }
-
-    public function testGetSyncFileTransferDirectionGetPercentProgress($localFile, $remoteFile)
-    {
-
-    }
-
-    public function testGetSyncFileTransferDirectionPutPercentProgress($remoteFile, $localFile)
-    {
-
-    }
-
-    public function testGetSyncFileTransferDirectionGetSizeProgress($localFile, $remoteFile)
-    {
-
-    }
-
-    public function testGetSyncFileTransferDirectionPutSizeProgress($remoteFile, $localFile)
-    {
-
-    }
-
-    public function testGetRemoteFileSize($remoteFile)
-    {
-
-    }
-
-    public function testGetLocalFileSize($localFile)
-    {
-
-    }
-
-    public function testHumanFilesize($bytes, $decimals = 2) {
-
-    }
-
-    ///////////////////////////////////
-
-    private function testIsDir($dir)
-    {
-
-    }
-
-    public function testFileExistGet($strFileName)
-    {
-
-    }
-
-    public function testFileExistPut($strFileName)
-    {
-
-    }
-
-    ///////////////////////////////////
-    public function testCd($strDirectory)
-    {
-
-    }
-
-    public function testDownload($strRemoteFile)
-    {
-
-    }
-
-    public function testUpload($strLocalFile)
-    {
-
-    }
-
-    /////////////////////////////////////////////
-    public function testSetOverwriteMode($strOverwriteMode)
-    {
-
-    }
-
-    public function testGetOverwriteMode()
-    {
-    }
-
-    public function testSetDefaultOverwriteMode()
-    {
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    public function testSetAction($strAction)
-    {
-
-    }
-
-    public function testGetAction()
-    {
-    }
-
-    public function testSetDefaultAction()
-    {
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    public function testSetDirection($strDirection)
-    {
-
-    }
-
-    public function testGetDirection()
-    {
-    }
-
-    public function testSetDefaultDirection()
-    {
-    }
-
-
-    /////////////////////////////////////////////
-    public function testSetSearchingFilesFilter($strRegex)
-    {
-
-    }
-
-    public function testGetSearchingFilesFilter()
-    {
-    }
-
-    public function testUnsetSearchingFilesFilter()
-    {
-    }
-
-    public function testIsSearchingFilesFilter()
-    {
-
-    }
-
-    public function testIsTriggeringSearchingFilesFilter($file)
-    {
-
-    }
-
-
-    /////////////////////////////////////////////
-    public function testSetRecursively($isRecursively)
-    {
-
-    }
-
-    public function testIsRecursively()
-    {
-
-    }
-
-    public function testSetDefaultRecursively()
-    {
-
-    }
-
-    //////////////////////////////////////////////
-    public function testTransferSkip()
-    {
-
-    }
-
-    public function testTransferRename()
-    {
-
-    }
-
-    public function testTransferOverwrite()
-    {
-
-    }
-
-    //////////////////////////////////////////////
-    public function testCallArgumentsStashing()
-    {
-
-    }
-
-    public function testCallArgumentsUnstashing()
-    {
-
-    }
 
 
 }
